2cef8125ac2d27483c08fb783bc4ae2e72253434
kanaderu0 <realuse@outlook.jp>
2026-03-18 23:53:33 +0900

    Update youtube.js

--- api/youtube.js
+++ api/youtube.js
@@ -1,4 +1,13 @@
 export default async function handler(req, res) {
+  // CORS 対応
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+  
   const API_KEY = process.env.API_KEY;
   const CHANNEL_ID = "UCGc93NguHRwzv1Rw9MyIcxQ";
 
